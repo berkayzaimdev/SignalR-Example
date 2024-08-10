089b5396d7b46d23ef35380f748ede352c3e2b65
berkayzaimdev <berkayzaimdev@gmail.com>
2024-08-10 15:56:44 +0300

    Added the project

---
+++ src/SignalR.API/ChatHub.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalR.API
+{
+    public sealed class ChatHub : Hub<IChatClient>
+    {
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.All.ReceiveMessage($"{Context.ConnectionId} has joined");
+        }
+
+        public async Task SendMessage(string message) // websocket isteği ile "target:SendMessage" diyebiliriz
+        {
+            //await Clients.All.SendAsync("ReceiveMessage",$"{Context.ConnectionId}: {message}"); // bu şekilde de kullanabiliriz, interface oluşturarak da
+            await Clients.All.ReceiveMessage($"{Context.ConnectionId}: {message}");
+        }
+    }
+
+    public interface IChatClient
+    {
+        Task ReceiveMessage(string message);
+    }
+}
